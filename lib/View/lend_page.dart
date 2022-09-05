797ae57482536ce2fe2d659a7d16fab0c6891357
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-05 22:48:40 +0800

    Added stablecoin cards which can be selected by user

--- lib/View/lend_page.dart
+++ lib/View/lend_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LendPage extends StatefulWidget {
-  const LendPage({Key? key}) : super(key: key);
+class LendPage extends StatelessWidget {
+  LendPage({Key? key}) : super(key: key);
 
-  @override
-  State<LendPage> createState() => _LendPageState();
-}
-
-class _LendPageState extends State<LendPage> {
   final _textController = TextEditingController();
   String userInput = "";
 
@@ -18,68 +13,62 @@ class _LendPageState extends State<LendPage> {
       padding: EdgeInsets.all(20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          getQuestionComponent(context),
-          getTextFieldComponent(context),
-          getSubmitButtonComponent(context),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.2,
-            width: MediaQuery.of(context).size.width,
-          )
+          getStableCoinWidgets(context, "USDT"),
+          getStableCoinWidgets(context, "USDC"),
+          getStableCoinWidgets(context, "DAI"),
+
         ],
       ),
     );
   }
 
-  Flexible getQuestionComponent(BuildContext context){
+  Flexible getStableCoinWidgets(BuildContext context, String stableCoin){
     return Flexible(
-        flex: 5,
-        fit: FlexFit.tight,
-        child: Center(
-            child: Text(
-                userInput,
-                style: TextStyle(fontSize: 35)
-            )
-        )
-    );
-  }
-
-  Flexible getTextFieldComponent(BuildContext context){
-    return Flexible(
-      flex: 3,
-      child: TextField(
-        controller: _textController,
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          hintText: "Amount of USDT to Borrow",
-          suffixIcon: IconButton(
-            onPressed: (){
-              _textController.clear();
-            },
-            icon: const Icon(Icons.clear),
+        flex: 3,
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
+          // color: stableCoin == "USDT" ?
+          //   Colors.green[300] :
+          //   stableCoin == "USDC" ?
+          //     Colors.blue[400] :
+          //     Colors.orange[300],
+          child: GestureDetector(
+            onTap: (){},
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: Card(
+                  elevation: 10,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                          child: Image.asset(
+                            stableCoin == "USDT" ?
+                            "assets/images/usdt.png" :
+                            stableCoin == "USDC" ?
+                            "assets/images/usdc.png" :
+                            "assets/images/dai.png",
+                          )
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                          stableCoin == "USDT" ?
+                          "USDT" :
+                          stableCoin == "USDC" ?
+                          "USDC" :
+                          "DAI",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600)
+                      ),
+                    ],
+                  )
+              ),
+            ),
           ),
-        ),
-      ),
-    );
-  }
-
-  Flexible getSubmitButtonComponent(BuildContext context) {
-    return Flexible(
-      flex: 1,
-      child: SizedBox(
-        child: CupertinoButton(
-          padding: const EdgeInsets.only(top: 15, bottom: 15, left: 25, right: 25),
-          onPressed: (){
-            setState(() {
-              userInput = _textController.text;
-              _textController.clear();
-            });
-          },
-          color: Colors.purple[300],
-          child: const Text("Submit"),
-        ),
-      ),
+        )
     );
   }
 }
