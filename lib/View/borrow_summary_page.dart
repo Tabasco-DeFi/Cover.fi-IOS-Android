5357cba2851f2170b794d5db4ab9dd81b9b3741c
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-06 20:48:46 +0800

    A summary page that will be used to summarize the user borrow form input state

--- lib/View/borrow_summary_page.dart
+++ lib/View/borrow_summary_page.dart
@@ -1,2 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+// Controller
+import 'package:coverfi_flutter/Controller/borrow_state_controller.dart';
+
+class BorrowSummaryPage extends StatelessWidget {
+  BorrowSummaryPage({Key? key}) : super(key: key);
+  final borrowStateController = Get.find<BorrowStateController>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        child: Center(
+            child: Text("${borrowStateController.loanCurrency.value} Borrow Summary Page")
+        ),
+      ),
+    );
+  }
+
+  // Stepper getStepper(){}
+}
