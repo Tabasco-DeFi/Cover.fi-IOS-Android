029d29ce8fb2148074daffb22a5ca0a25eacd7a7
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-06 23:47:51 +0800

    Added stepper state controller

--- lib/Controller/borrow_state_controller.dart
+++ lib/Controller/borrow_state_controller.dart
@@ -2,14 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class BorrowStateController extends GetxController{
+  // BorrowSummaryPage - State
+  RxInt step = 0.obs;
+  List<bool> isCompleted = List<bool>.filled(10, false, growable: false);
+
+  updateStep(int _step) {
+    step.value = _step;
+  }
+  updateCompletedStep(int index){
+    isCompleted[index] = true;
+  }
+
+
+
   // BorrowPage - State
   RxString loanCurrency = "".obs;
   RxInt targetLoanAmount = 0.obs;
   RxInt loanTenor = 0.obs;
+  RxString collateralType = "".obs;
   RxInt subscriptionPeriod = 0.obs;
   // subscription start date
   // subscription end date
-  RxString collateralCurrency = "".obs;
   RxDouble lowerProtectionRange = 0.0.obs;
   RxDouble upperProtectionRange = 0.0.obs;
   RxDouble loanRate = 0.0.obs;
