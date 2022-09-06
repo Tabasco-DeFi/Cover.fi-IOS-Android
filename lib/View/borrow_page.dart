c6552b74d2be7cc3b3146b6146f735875d4a4f12
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-06 20:48:01 +0800

    Code refactoring

---
+++ lib/View/borrow_page.dart
@@ -0,0 +1,95 @@
+// Packages
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// Controller
+import 'package:coverfi_flutter/Controller/borrow_state_controller.dart';
+
+// Views
+import 'package:coverfi_flutter/View/borrow_summary_page.dart';
+
+class BorrowPage extends StatelessWidget {
+  BorrowPage({Key? key}) : super(key: key);
+  final _textController = TextEditingController();
+  final borrowStateController = Get.find<BorrowStateController>();
+
+  String userInput = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(35),
+        child: AppBar(
+          title: Text("Borrow"),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            getStableCoinWidgets(context, "USDT"),
+            getStableCoinWidgets(context, "USDC"),
+            getStableCoinWidgets(context, "DAI"),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Flexible getStableCoinWidgets(BuildContext context, String stableCoin){
+    return Flexible(
+        flex: 3,
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
+          // color: stableCoin == "USDT" ?
+          //   Colors.green[300] :
+          //   stableCoin == "USDC" ?
+          //     Colors.blue[400] :
+          //     Colors.orange[300],
+          child: GestureDetector(
+            onTap: (){
+              borrowStateController.isFillingBorrowForm();
+              borrowStateController.updateLoanCurrency(stableCoin);
+              Get.to(BorrowSummaryPage());
+            },
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: Card(
+                  elevation: 10,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                          child: Image.asset(
+                            stableCoin == "USDT" ?
+                            "assets/images/usdt.png" :
+                            stableCoin == "USDC" ?
+                            "assets/images/usdc.png" :
+                            "assets/images/dai.png",
+                          )
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                          stableCoin == "USDT" ?
+                          "USDT" :
+                          stableCoin == "USDC" ?
+                          "USDC" :
+                          "DAI",
+                          style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600)
+                      ),
+                    ],
+                  )
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
