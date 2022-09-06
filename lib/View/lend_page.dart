3f0b3bcf756573c6f1c31ec0d9a37ed66b260db5
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-06 20:49:40 +0800

    Removed borrow page content

--- lib/View/lend_page.dart
+++ lib/View/lend_page.dart
@@ -2,73 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LendPage extends StatelessWidget {
-  LendPage({Key? key}) : super(key: key);
-
-  final _textController = TextEditingController();
-  String userInput = "";
+  const LendPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          getStableCoinWidgets(context, "USDT"),
-          getStableCoinWidgets(context, "USDC"),
-          getStableCoinWidgets(context, "DAI"),
-
-        ],
-      ),
-    );
-  }
-
-  Flexible getStableCoinWidgets(BuildContext context, String stableCoin){
-    return Flexible(
-        flex: 3,
-        child: Container(
-          margin: const EdgeInsets.all(10.0),
-          padding: const EdgeInsets.all(20.0),
-          // color: stableCoin == "USDT" ?
-          //   Colors.green[300] :
-          //   stableCoin == "USDC" ?
-          //     Colors.blue[400] :
-          //     Colors.orange[300],
-          child: GestureDetector(
-            onTap: (){},
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3,
-              child: Card(
-                  elevation: 10,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Center(
-                          child: Image.asset(
-                            stableCoin == "USDT" ?
-                            "assets/images/usdt.png" :
-                            stableCoin == "USDC" ?
-                            "assets/images/usdc.png" :
-                            "assets/images/dai.png",
-                          )
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                          stableCoin == "USDT" ?
-                          "USDT" :
-                          stableCoin == "USDC" ?
-                          "USDC" :
-                          "DAI",
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600)
-                      ),
-                    ],
-                  )
-              ),
-            ),
-          ),
-        )
+    return Center(
+      child: Text("Lending Page"),
     );
   }
 }
