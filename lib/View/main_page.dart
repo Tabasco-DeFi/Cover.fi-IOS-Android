87d2a812d94364322c7a219ba618f3f30cad4e34
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-06 20:50:12 +0800

    Added scaffold to app

--- lib/View/main_page.dart
+++ lib/View/main_page.dart
@@ -7,58 +7,69 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(35),
+        child: AppBar(
+          title: Text("Home"),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 10),
+        child: Column(
           children: <Widget>[
-            Expanded(
-                child: SizedBox(
-              height: 150,
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder: (BuildContext context, int index) {
-                  final List<String> summaries = [
-                    "Current TVL",
-                    "Total Loans Originated",
-                    "Total Protocol Revenue",
-                    "Total Tokens Burned"
-                  ];
-                  final List<int> values = [10000, 20000, 30000, 40000];
-                  return Card(
-                      elevation: 10,
-                      child: SizedBox(
-                        width: 150,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(6.0),
-                              child: Text(
-                                summaries[index],
-                                softWrap: true,
-                                overflow: TextOverflow.fade,
-                                maxLines: 2,
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    itemBuilder: (BuildContext context, int index) {
+                      final List<String> summaries = [
+                        "Current TVL",
+                        "Total Loans Originated",
+                        "Total Protocol Revenue",
+                        "Total Tokens Burned"
+                      ];
+                      final List<int> values = [10000, 20000, 30000, 40000];
+                      return Card(
+                          elevation: 10,
+                          child: SizedBox(
+                            width: 150,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(6.0),
+                                  child: Text(
+                                    summaries[index],
+                                    softWrap: true,
+                                    overflow: TextOverflow.fade,
+                                    maxLines: 2,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Text("\$${values[index]}")
+                              ],
                             ),
-                            Text("\$${values[index]}")
-                          ],
-                        ),
-                      ));
-                },
-              ),
-            ))
+                          ));
+                    },
+                  ),
+                ))
+              ],
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            const Center(child: Text("Info"))
           ],
         ),
-        const SizedBox(
-          height: 100,
-        ),
-        const Center(child: Text("Info"))
-      ],
+      ),
     );
   }
 }
