f9ce035febae70c2bb96fbd03e274ec26cd7bd93
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-06 20:51:08 +0800

    Added borrow summary to route

--- lib/main_routes.dart
+++ lib/main_routes.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 // Views
 import 'package:coverfi_flutter/View/home_page.dart';
 import 'package:coverfi_flutter/View/metamask_page.dart';
+import 'package:coverfi_flutter/View/borrow_summary_page.dart';
 
 appRoutes() => [
       GetPage(
@@ -16,5 +17,11 @@ appRoutes() => [
         page: () => MetaMaskWidget(),
         transition: Transition.leftToRightWithFade,
         transitionDuration: const Duration(milliseconds: 500),
+      ),
+      GetPage(
+        name: "/borrowPage",
+        page: () => BorrowSummaryPage(),
+        transition: Transition.leftToRightWithFade,
+        transitionDuration: const Duration(milliseconds: 500)
       )
     ];
