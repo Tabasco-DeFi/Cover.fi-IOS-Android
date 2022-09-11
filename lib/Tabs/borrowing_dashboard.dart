24bad65acf009f90de7cf66cda17d566817b1458
eqfwcev123 <dohyeonee95@hotmail.com>
2022-09-11 13:36:13 +0800

    Comleted Card design for each mock borrowing data

--- lib/Tabs/borrowing_dashboard.dart
+++ lib/Tabs/borrowing_dashboard.dart
@@ -8,81 +8,136 @@ import 'package:flutter/services.dart';
 
 class BorrowingDashboard extends StatefulWidget {
   const BorrowingDashboard({Key? key}) : super(key: key);
-
   @override
   State<BorrowingDashboard> createState() => _BorrowingDashboardState();
 }
 
 class _BorrowingDashboardState extends State<BorrowingDashboard> {
   final BorrowStateController stateController = Get.find<BorrowStateController>();
+  late Future<List<dynamic>> _borrowerData;
+
+  Future<List<dynamic>> fetchData() async {
+    final String jsonData = await rootBundle.loadString("assets/json/mock_borrow_data.json");
+    final result = json.decode(jsonData); // List<dynamic>
+    return result;
+  }
 
   @override
   void initState() {
     super.initState();
-    final result = fetchData();
-  }
-
-  Future<List<MockBorrowModel>> fetchData() async {
-    final String jsonData = await rootBundle.loadString("assets/json/mock_borrow_data.json");
-    final list = json.decode(jsonData) as List<dynamic>;
-    return list.map((e) => MockBorrowModel.fromJson(e)).toList();
+    _borrowerData = fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-        shrinkWrap: true,
-        children: <Widget>[]
-    );
+    return FutureBuilder(
+      // The fetchData() will be called each time the widget is reconstructed.
+      // Should be used to fetch data from blockchain later on
+      // If the fetchData() should be not be called every time, initialize the state using StatefulWidget [initState() | didChangeDependencies() | didUpdateWidget()]
+      future: _borrowerData,
+      // snapshot -> Most recent communication with future
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          if(snapshot.hasError) {
+            return const Text("Error");
+          } else if (snapshot.hasData) {
+            return SingleChildScrollView(
+              child: Column(
+                children: getListItems(snapshot.data),
+              ),
+            );
+          } else {
+            return const Text("Empty Data");
+          }
+        } else {
+            return Text("State : ${snapshot.connectionState}");
+        }
+      },
+      );
   }
 
-  List<Widget> getListItems() {
+  List<Widget> getListItems(List<dynamic>? data) {
     List<Widget> listItems = [];
-    var result = stateController.mockBorrowData;
-    // print("Result is ${result.value}");
 
-    // for(int i=0;i< result["data"].length; i++){
-    //   String poolState = result["data"][i]["poolState"];
-    //   int poolId = result["data"][i]["poolId"];
-    //   String borrowerAddress = result["data"][i]["borrowerAddress"];
-    //   String loanCurrency = result["data"][i]["loanCurrency"];
-    //   int borrowAmount = result["data"][i]["borrowAmount"];
-    //   int tenor = result["data"][i]["tenor"];
-    //   String subscriptionStart = result["data"][i]["subscriptionStart"];
-    //   String subscriptionEnd = result["data"][i]["subscriptionEnd"];
-    //   String termStart = result["data"][i]["termStart"];
-    //   String termEnd = result["data"][i]["termEnd"];
-    //   int lowerBound = result["data"][i]["lowerBound"];
-    //   int upperBound = result["data"][i]["upperBound"];
-    //   int loanRate = result["data"][i]["loanRate"];
-    //   int lenderCount = result["data"][i]["lenders"].length;
-    //
-    //   listItems.add(
-    //       Padding(
-    //         padding: const EdgeInsets.all(10),
-    //         child: Container(
-    //             height: 200,
-    //             decoration: BoxDecoration(
-    //                 color: Colors.purple[200],
-    //                 borderRadius: BorderRadius.circular(10)
-    //             ),
-    //             child: Column(
-    //               children: <Widget>[
-    //                 Text("$poolId"),
-    //                 Text(borrowerAddress),
-    //                 Text("$borrowAmount"),
-    //               ],
-    //             )
-    //         ),
-    //       )
-    //   );
-    // }
+    for(int i=0;i< data!.length; i++){
+      listItems.add(
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: getCustomCard(data[i])
 
-    print("complete");
+                ),
+            ),
+          )
+      );
+    }
     return listItems;
   }
 
+  Card getCustomCard(dynamic data){
+    return Card(
+      elevation: 10,
+      color: data["loanCurrency"] == "USDT" ?
+        Colors.greenAccent[100]:
+        (data["loanCurrency"] == "USDC" ? Colors.blueAccent[100]: Colors.yellowAccent[100]),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Borrow Amount", style: TextStyle(color: Colors.black)),
+                Image(
+                  image: data["loanCurrency"] == "USDT" ?
+                    const AssetImage("assets/images/usdt48.png"):
+                    data["loanCurrency"] == "USDC" ?
+                      const AssetImage("assets/images/usdc48.png"):
+                      const AssetImage("assets/images/dai48.png")
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Text("\$${data["borrowAmount"]} ${data["loanCurrency"]}", style: const TextStyle(color: Colors.black, fontSize: 24)),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(filterWalletAddress(data["borrowerAddress"]), style: const TextStyle(color: Colors.black)),
+                Row(
+                  children: [
+                    Column(children: const <Widget>[
+                      Text("Valid", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12)),
+                      Text("Thru", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12))
+                    ]),
+                    const SizedBox(width: 4),
+                    Text("${data["subscriptionEnd"]}"),
+                  ],
+                )
+              ],
+            )
 
+          ],
+        ),
+      ),
+    );
+  }
+
+  String filterWalletAddress(String address){
+    String firstDigits = address.substring(0,5);
+    String trailingDigts = address.substring(address.length-5);
+    return "$firstDigits******$trailingDigts";
+  }
 }
 
 
